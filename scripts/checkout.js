776e44fa6d70ef338fd139b27c2634c93eef6e91
Franklin Kuba <franklinkuba7@gmail.com>
2025-04-22 16:29:47 -0500

    Delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,10 +15,12 @@ cart.forEach((cartItem) => {
     }
   });
 
-  console.log(matchingProduct); //OR
+  // console.log(matchingProduct); //OR
   //   const matchingProduct = products.find((product) => product.id === productId);
 
-  cartSummaryHTML += `   <div class="cart-item-container">
+  cartSummaryHTML += `   <div class="cart-item-container  js-cart-item-container-${
+    matchingProduct.id
+  }">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -98,8 +100,14 @@ document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
-    removeFromCart(productId);
-    // console.log(cart);
+    removeFromCart(productId); // console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    // console.log(container);
+    container.remove();
+
     // updateCartQuantity();
     // updateOrderSummary();
   });
