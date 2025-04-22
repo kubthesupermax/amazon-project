4f9f75609e09870ba56b0d6e71e81f94d0f8d48b
Franklin Kuba <franklinkuba7@gmail.com>
2025-04-22 16:42:41 -0500

    Save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,22 @@
 // Research on data normalization / Normalizing the data
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   //Steps
@@ -33,6 +41,8 @@ export function addToCart(productId) {
       quantity: 1,
     }); //3.
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -45,4 +55,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newArray;
+
+  saveToStorage();
 }
